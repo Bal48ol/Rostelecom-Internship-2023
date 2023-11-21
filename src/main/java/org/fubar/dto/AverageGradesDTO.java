5123ae65eb6c41692399cb6bcad206add3b15d06
Bal48ol <day.of.travel69@gmail.com>
2023-11-21 22:09:04 +0300

    правки #4: Удалён ненужный ДТО + подчистил неиспользуемые зависимости

--- src/main/java/org/fubar/dto/AverageGradesDTO.java
+++
@@ -1,5 +0,0 @@
-package org.fubar.dto;
-
-public record AverageGradesDTO(double physics, double mathematics, double rus,
-                               double literature, double geometry, double informatics) {
-}
